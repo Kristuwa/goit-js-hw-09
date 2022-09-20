9965ce3ab1db89df2ce654335ce62f7db4ec5c71
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-20 21:22:52 +0000

    Deploying to gh-pages from @ Kristuwa/goit-js-hw-09@e7504d5cc50a20449f5fed92001c6a66691145e6 🚀

---
+++ 01-color-switcher.f13392b8.js
@@ -0,0 +1,2 @@
+let t=null;const n={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};n.startBtn.addEventListener("click",(function(){t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),n.startBtn.disabled=t})),n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.startBtn.disabled=!t}));
+//# sourceMappingURL=01-color-switcher.f13392b8.js.map
