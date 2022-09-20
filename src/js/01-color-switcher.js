2fcd80a45b7c252524692863f29efd2e2472ab15
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-20 22:52:31 +0300

    add file 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const randomColor = getRandomHexColor();
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stoptBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  setInterval(() => {
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function onStopBtnClick() {}
