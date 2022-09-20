a15adae7571e9950d0bea6373b5b6283854cdbb0
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-20 19:53:04 +0000

    Deploying to gh-pages from @ Kristuwa/goit-js-hw-09@2fcd80a45b7c252524692863f29efd2e2472ab15 🚀

---
+++ 01-color-switcher.b4f688db.js
@@ -0,0 +1,2 @@
+!function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16)),n={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};n.startBtn.addEventListener("click",(function(){setInterval((function(){document.body.style.backgroundColor=t}),1e3)})),n.stoptBtn.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.b4f688db.js.map
---
+++ 01-color-switcher.fb8f258b.js
@@ -0,0 +1,2 @@
+const t=`#${Math.floor(16777215*Math.random()).toString(16)}`,n={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};n.startBtn.addEventListener("click",(function(){setInterval((()=>{document.body.style.backgroundColor=t}),1e3)})),n.stoptBtn.addEventListener("click",(function(){}));
+//# sourceMappingURL=01-color-switcher.fb8f258b.js.map
